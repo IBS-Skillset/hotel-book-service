7a3206655adbfbd50825a969959456ace3f375d0
Aswathy V <v-avinjanan@expediagroup.com>
2022-12-01 11:37:48 +0530

    [UP][TECH-521] added token validation & request model

---
+++ src/main/java/com/hotel/api/book/interceptors/ApiKeyAuthInterceptor.java
@@ -0,0 +1,29 @@
+package com.hotel.api.book.interceptors;
+
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.ForwardingClientCall;
+import io.grpc.MethodDescriptor;
+import io.grpc.Metadata;
+import io.grpc.Channel;
+import io.grpc.CallOptions;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.client.interceptor.GrpcGlobalClientInterceptor;
+import org.slf4j.MDC;
+
+@GrpcGlobalClientInterceptor
+@Slf4j
+public class ApiKeyAuthInterceptor implements ClientInterceptor {
+    @Override
+    public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> methodDescriptor, CallOptions callOptions, Channel channel) {
+        log.info("client interceptor {}", methodDescriptor.getFullMethodName());
+        return new
+                ForwardingClientCall.SimpleForwardingClientCall<>(channel.newCall(methodDescriptor, callOptions)) {
+                    @Override
+                    public void start(Listener<RespT> responseListener, Metadata headers) {
+                    headers.put(Metadata.Key.of("accesstoken", Metadata.ASCII_STRING_MARSHALLER), MDC.get("token"));
+                    super.start(responseListener, headers);
+                    }
+                };
+    }
+}
