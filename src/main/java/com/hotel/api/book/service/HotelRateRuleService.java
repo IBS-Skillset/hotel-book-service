68bff8d84c087c74a00aaf1a6f2f4edf2bc8bf28
ArjunAruz <111626234+ArjunAruz@users.noreply.github.com>
2022-12-02 12:59:32 +0530

    Update HotelRateRuleService.java

--- src/main/java/com/hotel/api/book/service/HotelRateRuleService.java
+++ src/main/java/com/hotel/api/book/service/HotelRateRuleService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class HotelRateRuleService {
 
-    @GrpcClient("hotel-rate-pad")
+    @GrpcClient("hotel-book-reprice-pad")
     HotelRateRuleServiceGrpc.HotelRateRuleServiceBlockingStub hotelRateRuleServiceBlockingStub;
 
     @Autowired
