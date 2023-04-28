774a0586704e9a4d7830838b7e9f446dd2b01442
bkalarikkal <v-bkalarikkal@Traveldoo.com>
2023-04-28 12:45:27 +0530

    [AWS-DEPLOYMENT] - seperating docker file for quesry and cmd app

--- src/main/java/com/hotel/api/book/config/ResourceServerConfig.java
+++ src/main/java/com/hotel/api/book/config/ResourceServerConfig.java
@@ -4,15 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.beans.factory.annotation.Value;
 
 
 @Configuration
 public class ResourceServerConfig {
 
+    @Value("${auth.server.url}")
+    private String url;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.oauth2ResourceServer(
-                j -> j.jwt().jwkSetUri("http://localhost:9000/auth-server/oauth2/jwks")
+                j -> j.jwt().jwkSetUri(url + "/auth-server/oauth2/jwks")
         ).authorizeRequests()
                 .anyRequest().authenticated()
                 .and().build();
